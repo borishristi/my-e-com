2f96c1a89f3ef22b7153a3bce873328c82bfe717
Hristi <borisarroga@gmail.com>
2024-06-04 12:40:41 +0200

    I implement the base.html design for all pages. Il added the bootstrap framework link, javascript links, and other static files links

--- src/shop/views.py
+++ src/shop/views.py
@@ -4,8 +4,23 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def shop(request):
+def shop(request, *args, **kwargs):
     """Vue des produits"""
+
     context = {}
-    # return HttpRequest('I\'m testing')
+
     return render(request, 'shop/index.html', context)
+
+
+def panier(request, *args, **kwargs):
+
+    context = {}
+
+    return render(request, 'shop/panier.html', context)
+
+
+def commande(request, *args, **kwargs):
+
+    context = {}
+
+    return render(request, 'shop/commande.html', context)
---
+++ src/static/js/script.js
@@ -0,0 +1,5 @@
+// let monTest = document.querySelector('.test_css');
+
+document.querySelector('.test_css').textContent = 'Oui on JS passe';
+
+document.querySelector('.test_css').style.color = 'red';
