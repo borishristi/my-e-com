14d6df3fc5396a34094a7aea05c7e56916041fcc
Hristi <borisarroga@gmail.com>
2024-06-07 14:43:22 +0200

    Exécution de la commande pour un utilisateur Anonyme
    - Nous avons implémenté la fonction de sauvegarde de la commande d'un client anonyme

--- src/shop/models.py
+++ src/shop/models.py
@@ -13,7 +13,7 @@ class Client(models.Model):
     email = models.EmailField(max_length=200, null=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class Category(models.Model):
--- src/shop/views.py
+++ src/shop/views.py
@@ -126,35 +126,68 @@ def update_article(request, *args, **kwargs):
     return JsonResponse({'panier modifier': 'yes'}, safe=False)
 
 
+def commandeAnonyme(request, data):
+    name = data['form']['name']
+    print('data', data)
+    print('name', name)
+    username = data['form']['username']
+    email = data['form']['email']
+    phone = data['form']['phone']
+
+    cookie_panier = panier_cookie(request)
+
+    articles = cookie_panier['articles']
+
+    client, created = Client.objects.get_or_create(
+        email=email
+    )
+
+    client.name = name
+    client.save()
+
+    commande = Commande.objects.create(
+        client=client
+    )
+
+    for article in articles:
+        produit = Produit.objects.get(id=article['produit']['pk'])
+
+        CommandeArticle.objects.create(
+            produit=produit,
+            commande=commande,
+            quantite=article['quantite']
+        )
+
+    return client, commande
+
+
 def traitement_commande(request, *args, **kwargs):
     data = json.loads(request.body)
     transaction_id = datetime.now().timestamp()
 
     if request.user.is_authenticated:
         client = request.user.client
-
         commande, created = Commande.objects.get_or_create(client=client, complete=False)
 
-        print(f"Le total est : {data['form']['total']}")
-        print(type(data['form']['total']))
-        my_str = '24.77,00'
-        my_float = float(my_str.split(',')[0])
-        print(my_float)
-        total = float(data['form']['total'].split(",")[0])
-        commande.transaction_id = transaction_id
-        if commande.get_panier_total == total:
-            commande.complete = True
-            commande.save()
 
-        if commande.produit_physique:
-            AddressChipping.objects.create(
-                client=client,
-                commande=commande,
-                addresse=data['shipping']['address'],
-                ville=data['shipping']['city'],
-                zipcode=data['shipping']['zipcode']
-            )
     else:
-        print('utilisateur non connecté')
+        client, commande = commandeAnonyme(request, data)
+
+    total = float(data['form']['total'].split(",")[0])
+    commande.transaction_id = transaction_id
+
+    if commande.get_panier_total == total:
+        commande.complete = True
+
+    commande.save()
+
+    if commande.produit_physique:
+        AddressChipping.objects.create(
+            client=client,
+            commande=commande,
+            addresse=data['shipping']['address'],
+            ville=data['shipping']['city'],
+            zipcode=data['shipping']['zipcode']
+        )
 
     return JsonResponse('Traitement complet', safe=False)
