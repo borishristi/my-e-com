8b8c04d4934eaa41a7b80c4cd6847ef1752f4db7
Hristi <borisarroga@gmail.com>
2024-06-18 15:05:29 +0200

    Modification de la barre de navigation
    - Nous avons modifié l'ordre des éléments de la barre de navigateur
    - Nous avons changé les logos des comptes
    - Nous avons ajouté des éléments de text sous les logos
    - Nous avons créé un fichier css et un fichier js pour le design
    *************************
    
    Modifying the navigation bar
    
    - We've changed the order of navigation bar elements
    - Changed accounts logo
    - We have added text element under logo
    - We have created a css file and js file to the design

---
+++ src/static/js/menu.js
@@ -0,0 +1,22 @@
+let logo_div = document.getElementById('logo_div');
+// let search_div = document.getElementById('search_div');
+let search_div = document.querySelector('#search_div')
+let menu_div = document.getElementById('menu_div');
+let logo_user = document.getElementById('logo_user');
+let logo_user_link = document.getElementById('logo_user_link');
+let mon_panier = document.getElementById('mon_panier');
+
+
+// search_div.className = 'd-none';
+
+if (user == 'AnonymousUser') {
+    logo_user.className = 'fa-solid fa-right-to-bracket fa-xl pb-0 mt-3 border';
+    mon_panier.innerText = 'Connexion';
+    mon_panier.style.float = 'center';
+    mon_panier.style.marginLeft = '7px';
+    logo_user_link.className = 'px-0 pt-2 px-lg-4 position-relative border';
+    logo_user_link.style.textDecoration = 'none';
+} else {
+    logo_user.className = 'fa-solid fa-user-check fa-xl pb-0 mt-3';
+    logo_user_link.className = 'px-2 pt-2 px-lg-4 position-relative';
+}
